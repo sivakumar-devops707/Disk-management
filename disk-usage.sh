b4b409cc220ec12f277e5effc2ee9345b5a755a3
sivakumar <sivakumarthalla123@gmail.com>
2026-02-07 18:26:37 +0530

    variables

---
+++ disk-usage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+log(){
+    echo "$(date "+%y %m %d %h %m %s") | $1" |tee -a $log_file
+}
+#df -hT | grep -v Filesystem | awk '{print $6}' |cut -d "%" -f1
+diskusage=$(df -hT | grep -v Filesystem)
+throsholdvalue=3
+message=""
+
+while IFS= read -r line
+do
+   usage=$( echo $line| awk '{print $6}' |cut -d "%" -f1)
+   partistion=$( echo $line| awk '{print $7}')
+
+    if [ $usage -gt $throsholdvalue ]; then
+       $message+="High disk usage on $partistion:$usage"
+    fi    
+
+done <<< $diskusage
+
+echo "$message";
